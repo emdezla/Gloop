9ca541e952e3d372b89d457d6e0fdba4c3ca4d06
Emilio Fernández <emdezla@gmail.com>
2025-03-29 10:18:27 -0500

    hola

--- SACCQL_training.py
+++ SACCQL_training.py
@@ -660,8 +660,9 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(torch.cuda.is_available())
     print(f"Training on device: {device}")
-    
+    print(torch.cuda.FloatTensor())
     # Train the agent
     agent = train_sac(
         dataset_path=args.dataset,
